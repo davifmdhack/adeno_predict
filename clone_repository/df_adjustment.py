7e30e1f43ada54f3b8e1c5d9b4966499def18d99
davi_fs <109975635+davifmdhack@users.noreply.github.com>
2024-08-16 12:13:55 -0300

    Create df_adjustment.py

---
+++ clone_repository/df_adjustment.py
@@ -0,0 +1,7 @@
+pip install -r requirements.txt -q --no-warn-script-location --no-warn-conflicts
+
+from tkinter import Tk, messagebox
+from tkinter import messagebox
+import pandas as pd
+import numpy as np
+
