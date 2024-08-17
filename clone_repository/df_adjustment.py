c23b64212fbeb192f32129d05c45154a747337b8
davi_fs <109975635+davifmdhack@users.noreply.github.com>
2024-08-17 12:28:04 -0300

    Delete clone_repository/df_adjustment.py

--- clone_repository/df_adjustment.py
+++
@@ -1,7 +0,0 @@
-pip install -r requirements.txt -q --no-warn-script-location --no-warn-conflicts
-
-from tkinter import Tk, messagebox
-from tkinter import messagebox
-import pandas as pd
-import numpy as np
-
